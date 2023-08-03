7228ab9b10d6735084dc3b53a53f920c0705e00e
datajcthemax <data.jcthemax@gmail.com>
2023-08-03 12:30:35 +0900

    글작성시 조건달기

--- pages/api/post/new.js
+++ pages/api/post/new.js
@@ -1,9 +1,19 @@
 import { connectDB } from "@/util/database"
 
-export default async function handler(req, res) {
-    if(req.method == 'POST') {
-        const db = (await connectDB).db('nextforum')
-        let result = await db.collection('post').insertOne(req.body)
-        return res.status(200).redirect('/list')
+export default async function handler(요청, 응답) {
+  if (요청.method == 'POST'){
+    if (요청.body.title == '') {
+      return 응답.status(500).json('제목써라')
+    } else if (요청.body.content == '') {
+        return 응답.status(500).json('내용써라')
     }
-}
+    try {
+      let db = (await connectDB).db('nextforum')
+      let result = db.collection('post').insertOne(요청.body)
+      응답.redirect(302, '/list')
+    } catch (error) {
+      return 응답.status(500).json('뭔가 에러남')
+    }
+    
+  }
+} 
